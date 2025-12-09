2f5ac2c6b00ca8a3bd4a33d0427352891938fa00
Lena <lena.dolinska@gmail.com>
2025-12-09 10:41:32 +0100

    zrobione backsubstr.c
    Lena

--- backsubst.c
+++ backsubst.c
@@ -5,18 +5,31 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	// r to liczba wierszy
+	// c to liczba kolumn
 
-				return 0;
+	int n = mat->r;
+
+	// musi byc kwadratowa
+	if (mat->r != mat->c || mat->r != b->r || b->c != 1 || x->r != b->r || x->c != 1) {
+		return 2;
+	}
+
+	for(int i = n-1; i >= 0; i--) {
+		// dzielenie przez 0
+		if (mat->data[i][i] == 0) {
+			return 1;
+		}
+
+		// obliczanie
+		x->data[i][0] = b->data[i][0];
+		for(int j = i + 1; j < n; j++) {
+			x->data[i][0] -= mat->data[i][j] * x->data[j][0];
+		}
+		x->data[i][0] /= mat->data[i][i];
+	}
+
+	return 0;
 }
-
-
